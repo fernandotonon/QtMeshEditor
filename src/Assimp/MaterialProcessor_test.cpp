93c3238b5becce8f8547bcb194a0a760eb313d3a
Fernando <tonon.fernando@hotmail.com>
2023-08-22 14:32:32 -0400

    fix test

--- src/Assimp/MaterialProcessor_test.cpp
+++ src/Assimp/MaterialProcessor_test.cpp
@@ -2,30 +2,9 @@
 #include <gmock/gmock.h>
 #include "MaterialProcessor.h"
 
-// Mock classes for Assimp's aiScene
-class MockAiScene : public aiScene {
-public:
-    unsigned int mNumMaterials;
-    aiMaterial** mMaterials;
-};
-
-// Test suite for MaterialProcessor
-class MaterialProcessorTest : public ::testing::Test {
-protected:
+TEST(MaterialProcessorTest, LoadSceneProcessesAllMaterials) {
     MaterialProcessor processor;
-
-    // Add any setup or tear down methods if needed
-    void SetUp() override {
-        // Setup code if needed
-    }
-
-    void TearDown() override {
-        // Cleanup code if needed
-    }
-};
-
-TEST_F(MaterialProcessorTest, LoadSceneProcessesAllMaterials) {
-    MockAiScene scene;
+    aiScene scene;
     aiMaterial mockMaterial1, mockMaterial2;
 
     scene.mNumMaterials = 2;
@@ -37,8 +16,9 @@ TEST_F(MaterialProcessorTest, LoadSceneProcessesAllMaterials) {
     EXPECT_EQ(processor.size(), 2);
 }
 
-TEST_F(MaterialProcessorTest, MaterialIndexing) {
-    MockAiScene scene;
+TEST(MaterialProcessorTest, MaterialIndexing) {
+    MaterialProcessor processor;
+    aiScene scene;
     aiMaterial mockMaterial1, mockMaterial2;
 
     scene.mNumMaterials = 2;
@@ -51,8 +31,9 @@ TEST_F(MaterialProcessorTest, MaterialIndexing) {
     EXPECT_NE(processor[1], nullptr);
 }
 
-TEST_F(MaterialProcessorTest, MaterialSize) {
-    MockAiScene scene;
+TEST(MaterialProcessorTest, MaterialSize) {
+    MaterialProcessor processor;
+    aiScene scene;
     aiMaterial mockMaterial1, mockMaterial2, mockMaterial3;
 
     scene.mNumMaterials = 3;
