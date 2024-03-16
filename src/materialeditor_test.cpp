9661464e3f84009b19c3e6814cd9a6be44cf9475
Fernando <tonon.fernando@hotmail.com>
2024-03-15 22:15:29 -0400

    try to fix tests

--- src/materialeditor_test.cpp
+++ src/materialeditor_test.cpp
@@ -6,20 +6,18 @@
 
 class MaterialEditorTest : public ::testing::Test {
 protected:
-    QApplication* app;
-
     void SetUp() override {
-        int argc = 0;
-        char* argv[] = { nullptr };
-        app = new QApplication(argc, argv);
     }
 
     void TearDown() override {
-        delete app;
     }
 };
 
 TEST_F(MaterialEditorTest, SetMaterialTextTest) {
+    int argc = 0;
+    char* argv[] = { nullptr };
+    QApplication app(argc, argv);
+
     std::unique_ptr<MaterialEditor> editor;
     editor = std::make_unique<MaterialEditor>();
 
