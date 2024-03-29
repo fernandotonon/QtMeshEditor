5ebf1587c1202eae13549003fd195962b204d5a9
Fernando <tonon.fernando@hotmail.com>
2024-03-29 12:23:41 -0400

    add tests for mainwindow

--- src/mainwindow_test.cpp
+++ src/mainwindow_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <QApplication>
+#include <QSettings>
 #include "mainwindow.h"
 
 class MainWindowTest : public ::testing::Test {
@@ -22,19 +23,71 @@ protected:
     }
 };
 
-TEST_F(MainWindowTest, ChooseCustomPalette) {
-    auto customPaletteAction = mainWindow->findChild<QAction*>("actionCustom");
-    ASSERT_TRUE(customPaletteAction != nullptr);
+TEST_F(MainWindowTest, ChooseDarkPalette) {
+    auto paletteAction = mainWindow->findChild<QAction*>("actionDark");
+    ASSERT_TRUE(paletteAction != nullptr);
 
     // Trigger the action
-    customPaletteAction->toggle();
+    paletteAction->toggle();
+
+    // There's no unchecking
+    paletteAction->toggle();
+    ASSERT_TRUE(paletteAction->isChecked());
+
+    QSettings settings;
+    auto selectedFalette = settings.value("palette");
+    EXPECT_EQ(selectedFalette, "dark");
+}
+
+TEST_F(MainWindowTest, ChooseLightPalette) {
+    auto paletteAction = mainWindow->findChild<QAction*>("actionLight");
+    ASSERT_TRUE(paletteAction != nullptr);
+
+    // Trigger the action
+    paletteAction->toggle();
+
+    // There's no unchecking
+    paletteAction->toggle();
+    ASSERT_TRUE(paletteAction->isChecked());
+
+    QSettings settings;
+    auto selectedFalette = settings.value("palette");
+    EXPECT_EQ(selectedFalette, "light");
+}
+
+TEST_F(MainWindowTest, ChooseCustomPalette) {
+    auto paletteAction = mainWindow->findChild<QAction*>("actionCustom");
+    ASSERT_TRUE(paletteAction != nullptr);
+
+    // Trigger the action
+    paletteAction->toggle();
 
     // Check if the color dialog is open
     auto colorDialog = mainWindow->getCustomPaletteColorDialog();
     ASSERT_TRUE(colorDialog != nullptr);
-
-    // Check if the color dialog is visible
     ASSERT_TRUE(colorDialog->isVisible());
+
+    colorDialog->setCurrentColor(QColor(125,122,123));
+    colorDialog->accept();
+
+    QSettings settings;
+    auto selectedPalette = settings.value("palette");
+    EXPECT_EQ(selectedPalette, "custom");
+
+    auto customPaletteColor = settings.value("customPalette");
+    EXPECT_EQ(customPaletteColor, QColor(125,122,123));
+
+    // There's no unchecking
+    paletteAction->toggle();
+    ASSERT_TRUE(paletteAction->isChecked());
 }
 
+TEST_F(MainWindowTest, ChooseAmbientLight) {
+    auto actionButton = mainWindow->findChild<QAction*>("actionChange_Ambient_Light");
+    ASSERT_TRUE(actionButton != nullptr);
 
+    // Trigger the action
+    actionButton->toggle();
+
+    // finish testing
+}
